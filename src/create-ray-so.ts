87cb3043df31ca9ffe9abd24005afe82a5d65cbf
TinsFox <fox@tinsfox.com>
2024-11-09 16:44:58 +0800

    chore: remove comment

--- src/create-ray-so.ts
+++ src/create-ray-so.ts
@@ -5,10 +5,8 @@ use scripting additions
 
 set theWindow to missing value
 
--- Can only display windows on the main thread
 my performSelectorOnMainThread:"showWindow" withObject:(missing value) waitUntilDone:true
 
--- Create and display the window
 on showWindow()
 	global theWindow
 	set theContentRect to current application's NSMakeRect(0, 0, 900, 600)
@@ -20,16 +18,13 @@ on showWindow()
 	theWindow's setMovableByWindowBackground:true
 	theWindow's setTitlebarAppearsTransparent:true
 
-	-- 创建 WebView
 	set webViewFrame to current application's NSMakeRect(0, 0, 900, 600)
 	set webView to current application's WKWebView's alloc()'s initWithFrame:webViewFrame
 
-	-- 创建 URL 请求并加载
 	set targetURL to current application's NSURL's URLWithString:"${url}"
 	set urlRequest to current application's NSURLRequest's requestWithURL:targetURL
 	webView's loadRequest:urlRequest
 
-	-- 将 WebView 添加到窗口
 	theWindow's contentView()'s addSubview:webView
 
 	theWindow's setLevel:(current application's NSFloatingWindowLevel)
@@ -37,7 +32,6 @@ on showWindow()
 	current application's NSApp's runModalForWindow:theWindow
 end showWindow
 
--- Stop the modal (and associated processes) when the window is closed
 on closeWindow:sender
 	current application's NSApp's stopModal()
 	theWindow's |close|()
@@ -47,7 +41,6 @@ on windowWillClose:theNotification
 	current application's NSApp's stopModal()
 end windowWillClose:
 
--- Keep the window on top
 on windowDidBecomeKey:theNotification
 	global theWindow
 	theWindow's setLevel:(current application's NSFloatingWindowLevel)
